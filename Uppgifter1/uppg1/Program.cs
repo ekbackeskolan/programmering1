46484d05166013578f4fc509ee7a648286e10135
rogersundekbackeskolan <roger.sund@ekbackeskolan.se>
2020-02-10 01:06:27 +0100

    Ver2

---
+++ Uppgifter1/uppg1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uppg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in ett tal, tack: ");
+            double tal1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hejsan! "+tal1);
+
+        }
+    }
+}
