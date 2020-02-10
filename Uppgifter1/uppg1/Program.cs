fd0afcc47dcba08ca737cd365132bd7195ac066c
rogersundekbackeskolan <roger.sund@ekbackeskolan.se>
2020-02-10 01:10:18 +0100

    Klart

--- Uppgifter1/uppg1/Program.cs
+++ Uppgifter1/uppg1/Program.cs
@@ -8,7 +8,11 @@ namespace uppg1
         {
             Console.Write("Mata in ett tal, tack: ");
             double tal1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Hejsan! "+tal1);
+            Console.Write("Mata in ett tal, tack: ");
+            double tal2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Mata in ett tal, tack: ");
+            double tal3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hejsan! Du skrev " + tal1 +", " + tal2 + ", " + tal3 );
 
         }
     }
